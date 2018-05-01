0fc729d2d364d64e5d92cb172b71e7e257c03e1d
mawrkus <web@sparring-partner.be>
2018-05-01 16:09:48 +0200

    Improved HTTP log

--- es/HttpClient.js
+++ es/HttpClient.js
@@ -1,16 +1,33 @@
 const axios = require('axios');
 const debug = require('debug')('holded:http');
 
-function logRequest(config) {
+function logRequest({ config, includeData = true, includeHeaders = false }) {
   const { method, baseURL, url, headers, data } = config;
-  debug('%s %s%s...', method.toUpperCase(), baseURL, url, data);
-  debug(headers);
+
+  debug('%s %s%s...', method.toUpperCase(), baseURL, url);
+
+  if (includeData) {
+    debug(data);
+  }
+
+  if (includeHeaders) {
+    debug(headers);
+  }
 }
 
-function logResponse(response) {
-  const { config, status, statusText, data } = response;
+function logResponse({ response, includeData = true, includeHeaders = false }) {
+  const { config, status, statusText, data, headers } = response;
   const { method, url } = config;
-  debug('%s %s: %s (%d)', method.toUpperCase(), url, statusText, status, data);
+
+  debug('%s %s: %s (%d)', method.toUpperCase(), url, statusText, status);
+
+  if (includeData) {
+    debug(data);
+  }
+
+  if (includeHeaders) {
+    debug(headers);
+  }
 }
 
 module.exports = class HttpClient {
@@ -21,7 +38,7 @@ module.exports = class HttpClient {
     const httpClient = axios.create(httpOptions);
 
     httpClient.interceptors.request.use((config) => {
-      logRequest(config);
+      logRequest({ config, includeHeaders: true });
       return config;
     }, async (error) => {
       debug('HTTP request error');
@@ -30,11 +47,11 @@ module.exports = class HttpClient {
     });
 
     httpClient.interceptors.response.use((response) => {
-      logResponse(response);
+      logResponse({ response });
       return response;
     }, async (error) => {
       const { response } = error;
-      logResponse(response);
+      logResponse({ response });
       throw error;
     });
 
